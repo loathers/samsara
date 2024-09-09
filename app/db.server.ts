7541da8e75b85619a2c26c7cd7d5fdaa46fa24aa
Sam Gaus <sam@gaus.co.uk>
2024-09-09 11:53:56 +0100

    Fix having too many clients in dev environment

--- app/db.server.ts
+++ app/db.server.ts
@@ -11,14 +11,27 @@ import { calculateRange } from "./utils";
 
 export const NS13 = "2007-06-25";
 
-const prisma = new PrismaClient({
-  log: [
-    {
-      emit: "event",
-      level: "query",
-    },
-  ],
-});
+declare global {
+  var globalPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.globalPrisma) {
+    global.globalPrisma = new PrismaClient({
+      log: [
+        {
+          emit: "event",
+          level: "query",
+        },
+      ],
+    });
+  }
+  prisma = global.globalPrisma;
+}
 
 // prisma.$on("query", async (e) => {
 //   console.log(`${e.query} ${e.params}`);
