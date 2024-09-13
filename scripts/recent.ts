2a2ea8424691e0de3eba0eebe41e295e56d0f511
Sam Gaus <sam@gaus.co.uk>
2024-09-13 23:19:09 +0100

    A tiny bit more loggign

--- scripts/recent.ts
+++ scripts/recent.ts
@@ -2,7 +2,7 @@ import { Player } from "@prisma/client";
 import { processAscensions, nextUpdateIn, workers } from "./utils/client.js";
 import { parseRecentAscenders } from "./utils/utils.js";
 
-const kol = workers[0];
+const client = workers[0];
 
 function* players(p: Player[]) {
   for (const player of p) yield player.id;
@@ -10,11 +10,13 @@ function* players(p: Player[]) {
 
 async function main() {
   console.time("etl");
-  const recent = await kol.fetchText(
+  console.timeLog("etl", "Begin")
+
+  const recent = await client.fetchText(
     "museum.php?place=leaderboards&whichboard=999&showhist=500",
   );
 
-  if (kol.isRollover()) {
+  if (client.isRollover()) {
     console.timeLog("etl", "Rollover detected, exiting");
     console.timeEnd("etl");
     return;
@@ -22,9 +24,12 @@ async function main() {
 
   const ascenders = parseRecentAscenders(recent);
 
+  console.timeLog("etl", `Found ${ascenders.length} ascenders`);
+
   await processAscensions(players(ascenders), false);
 
   await nextUpdateIn(Number(process.env.SCHEDULE || 1800));
+  console.timeLog("etl", "Done");
   console.timeEnd("etl");
 }
 
