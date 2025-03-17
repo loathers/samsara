30e8b06d49396a027ca16f7ac6a82fbf95a29986
Sam Gaus <sam@gaus.co.uk>
2025-03-17 13:11:02 +0000

    Format lifestyle

--- app/routes/records[.rss].ts
+++ app/routes/records[.rss].ts
@@ -1,6 +1,7 @@
 import { Feed } from "feed";
 import { data } from "react-router";
 
+import { formatLifestyle } from "~/components/Lifestyle";
 import { db, getMaxAge } from "~/db.server";
 import { hasExtra } from "~/utils";
 
@@ -43,7 +44,7 @@ export const loader = async () => {
           .map(([key, value]) => `${value} ${key}`)
           .join(", ")
       : `${record.turns}/${record.days}`;
-    const description = `${record.player.name} (#${record.player.id}) has achieved the best ${record.lifestyle} ${record.path.name} with ${score}`;
+    const description = `${record.player.name} (#${record.player.id}) has achieved the best ${formatLifestyle(record.lifestyle)} ${record.path.name} with ${score}`;
     feed.addItem({
       title: description,
       id: `https://samsara.loathers.net/player/${record.player.id}#${record.ascensionNumber}`,
