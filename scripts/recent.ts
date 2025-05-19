98379d1d289131156e15d838f9a2e9a680e1393a
Sam Gaus <sam@gaus.co.uk>
2025-05-19 13:20:01 +0100

    Better lop

--- scripts/recent.ts
+++ scripts/recent.ts
@@ -3,8 +3,6 @@ import { Player } from "@prisma/client";
 import { nextUpdateIn, processAscensions, workers } from "./utils/client.js";
 import { parseRecentAscenders } from "./utils/utils.js";
 
-const client = workers[0];
-
 function* players(p: Player[]) {
   for (const player of p) yield player.id;
 }
@@ -16,15 +14,16 @@ async function main() {
   let recent = "";
   let i = 0;
   while (recent === "" && i < 10) {
-    recent = await workers[i++ % workers.length].fetchText(
+    const client = workers[i++ % workers.length];
+    recent = await client.fetchText(
       "museum.php?place=leaderboards&whichboard=999&showhist=500",
     );
-  }
 
-  if (client.isRollover()) {
-    console.timeLog("etl", "Rollover detected, exiting");
-    console.timeEnd("etl");
-    return;
+    if (recent !== "" && client.isRollover()) {
+      console.timeLog("etl", "Rollover detected, exiting");
+      console.timeEnd("etl");
+      return;
+    }
   }
 
   const ascenders = parseRecentAscenders(recent);
