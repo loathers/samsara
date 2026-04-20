51a399e0b1e61614efb8eb64a1352bd531b263a0
Sam Gaus <sam@gaus.co.uk>
2026-04-20 10:07:49 +0100

    Upgrade deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import eslint from "@eslint/js";
 import react from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   react.configs.flat["jsx-runtime"],
